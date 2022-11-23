1a1cfee62dda0bb72b0ead15c9601e2bc52d8b12
Artem Mirvoda <mirvodartem@gmail.com>
2022-11-24 03:26:53 +0500

    Refactor bbc_parser.py

--- source/bbc_parser.py
+++ source/bbc_parser.py
@@ -1,48 +1,24 @@
-import pandas as pd
-import requests
 from bs4 import BeautifulSoup
-import re
 
-ARTICLES = 'data\\articles.csv'
-smpl_matched_links_equile_year = pd.read_csv('data\\smpl_matched_links_equile_year.csv', index_col=0)
-articles = pd.DataFrame({'id': '',
-                         'tj_article_title': '',
-                         'tj_article_introduction': '',
-                         'tj_article_text': [],
-                         'persian_article_title': '',
-                         'persian_article_introduction': '',
-                         'persian__article_text': []
-                         })
+class BBCParser:
 
-def Tj_bbc(url):
-    # пример: url = 'https://www.bbc.com/tajik/news/2015/03/150315_l16_l57_kerry_assad_negotiate'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    article_title = soup.find_all('h1', class_='story-body__h1')
-    article_introduction = soup.find_all('p', class_='story-body__introduction')
-    article_text = soup.find_all('div', class_='story-body__inner')
-    text = (re.split('</p><p*>', str(article_text)))[1:-1]
-    return [article_title[0].text, article_introduction[0].text, text]
+    @staticmethod
+    def __parse_text(content: bytes, select_query: str) -> list[str]:
+        soup = BeautifulSoup(content, features='html.parser')
+        text = []
+        text_nodes = soup.select(select_query)
+        for node in text_nodes:
+            if len(node.text) > 0:
+                text.append(node.text)
+        return text
+    
+    @staticmethod
+    def parse_text_tj(content: bytes) -> list[str]:
+        parsed_text = BBCParser.__parse_text(content=content, select_query='[property=articleBody] > p')
+        return parsed_text
 
-    # print(article_text[0])
-
-
-def Persian_bbc(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    article_title = soup.find_all('h1', class_='bbc-1gvq3vt e1p3vdyi0')
-    article_introduction = soup.find_all('p', class_='bbc-yabuuk e17g058b0')
-    article_text = soup.find_all('p', class_='bbc-yabuuk e17g058b0')
-    text = list((o.text for o in article_text))[1:]  # Элемент№0 это article_title. Из-за арабского языка(справа налево)
-    text.reverse()
-    return [article_title[0].text, article_introduction[0].text, text]
-
-
-for index, row in smpl_matched_links_equile_year.iterrows():
-    temp = ['','','','','','','']
-    try:
-        temp = ([str(row['id'])] + Tj_bbc(row['link']) + Persian_bbc(row['equals_persian_links'][2:-2]))  # Уберем [' и ']
-    except Exception:
-        print(Exception)
-    articles.loc[len(articles)] = temp
-    articles.to_csv(ARTICLES)
+    @staticmethod
+    def parse_text_pers(content: bytes) -> list[str]:
+        parsed_text = BBCParser.__parse_text(content=content, select_query='main > div > p')
+        if len(parsed_text) == 0: parsed_text = BBCParser.__parse_text(content=content, select_query='.bodytext > p')
+        return parsed_text
